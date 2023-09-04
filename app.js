ec33520506e84288f80de435f8d60b2c7c8ea8c1
EWRC01 <edwin1026@hotmail.es>
2023-09-04 11:45:43 -0600

    Try Google maps alternatives

--- app.js
+++ app.js
@@ -26,7 +26,29 @@ async function initMap() {
     }
 }
 
-// Call loadGoogleMapsScript when the page loads
-window.onload = function () {
-    loadGoogleMapsScript();
-};
+// Function to perform a location search on the map using Nominatim via HTTP request
+function searchTest() {
+    const searchInput = document.getElementById("searchInput").value;
+
+    // Use Nominatim for geocoding via HTTP request
+    fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${searchInput}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.length > 0) {
+                const location = data[0];
+
+                // Pan the map to the searched location
+                map.panTo({ lat: parseFloat(location.lat), lng: parseFloat(location.lon) });
+            } else {
+                console.error("Location not found.");
+                // Handle error here, e.g., display a message to the user
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            // Handle error here
+        });
+}
+
+// Add an event listener to the Search button
+document.getElementById("searchButton").addEventListener("click", searchTest);
