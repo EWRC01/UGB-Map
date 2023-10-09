4446a94ad3e85c98b36b34d48261c3b373b5155d
EWRC01 <edwin1026@hotmail.es>
2023-10-08 22:01:31 -0600

    Add a layer to modify the base layer of map

--- modules/map.js
+++ modules/map.js
@@ -1,6 +1,26 @@
+// Create the map
 var map = L.map('map').setView([13.48861, -88.19208], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 21,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// Create different tile layers
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,});
+
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,});
+
+var osmTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,});
+
+// Add one of the base layers to the map by default
+osm.addTo(map);
+
+// Create a base layers object
+var baseLayers = {
+    "Leaflet Default": osm,
+    "Leaflet Hot": osmHOT,
+    "Leaflet TopoMap" : osmTopoMap,
+};
+
+var layerControl = L.control.layers(baseLayers).addTo(map);
+
+layerControl.setPosition('bottomleft');
