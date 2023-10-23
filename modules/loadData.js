c26b7f2a509a726c5dbdc70149876d964995bc7d
EWRC01 <edwin1026@hotmail.es>
2023-10-23 10:19:08 -0600

    Change the way to display the buildings to a huge list with the name of classrooms

--- modules/loadData.js
+++ modules/loadData.js
@@ -1,36 +1,252 @@
 const buildingsData = [
+
     {
-        name: "Laboratorio Dennis Ritchie",
-        coordinates: "13.48850,-88.19343"
+        name:"Edificios Independientes",
+        "rooms" : [
+            {
+                "name": "Admisiones Universidad Gerardo Barrios",
+                "coordinates": "13.4889261, -88.1932690"
+            },
+            {
+                "name": "Auditorio Universidad Gerardo Barrios",
+                "coordinates": "13.4885195, -88.1928500"
+            },
+            {
+                "name": "Bienestar Estudiantil Universidad Gerardo Barrios",
+                "coordinates": "13.4886101, -88.1933524"
+            }
+        ]
     },
     {
+        name: "Laboratorio Dennis Ritchie",
+        "rooms" : [
+            {
+                "name":"LAB_C1_DR",
+                "coordinates" : "13.48850,-88.19343"
+            },
+            {
+                "name":"LAB_C2_DR",
+                "coordinates" : "13.48850,-88.19343"
+            },
+            {
+                "name":"LAB_C3_DR",
+                "coordinates" : "13.48850,-88.19343"
+            },
+            {
+                "name":"LAB_C4_DR",
+                "coordinates" : "13.48850,-88.19343"
+            },
+            {
+                "name":"LAB_HR_DR",
+                "coordinates" : "13.48850,-88.19343"
+            },
+
+        ]
+    },
+    
+    {
         name: "Edificio Innova",
-        coordinates: "13.4886243,-88.1936105"
+        "rooms" : [
+            {
+                "name":"INV_N1_LS",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+            {
+                "name":"INV_N1_LH",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+            {
+                "name":"INV_N2_SSDC",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+            {
+                "name":"INV_N2_CDS",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+            {
+                "name":"INV_N2_LM",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+            {
+                "name":"INV_N3_CDN",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+            {
+                "name":"INV_N3_TDA",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+            {
+                "name":"INV_N3_LSP",
+                "coordinates" : "13.4886243,-88.1936105"
+            },
+
+        ]
     },
     {
         name: "Edificio Juan Jose Cañas",
-        coordinates: "13.4886574,-88.1938024"
+        "rooms" : [
+            {
+                "name":"JC_N1_01",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N1_L.C.C",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N1_LA",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N2_LI_1",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N2_Magna_01",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N2_02",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N3_01",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N3_02",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N3_03",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+            {
+                "name":"JC_N3_04",
+                "coordinates" : "13.4886574,-88.1938024"
+            },
+        ]
     },
     {
-        name: "Edificio Hugo Lindo",
-        coordinates: "13.4896262, -88.1930545"
+        name: "Edificio Dr. Hugo Lindo",
+        "rooms": [
+            {
+                "name":"HL_N1_LI_2",
+                "coordinates" : "13.4896262, -88.1930545"
+            },
+            {
+                "name":"HL_N1_LE",
+                "coordinates" : "13.4896262, -88.1930545"
+            },
+            {
+                "name":"HL_N1_01",
+                "coordinates" : "13.4896262, -88.1930545"
+            },
+            {
+                "name":"HL_N1_02",
+                "coordinates" : "13.4896262, -88.1930545"
+            },
+            {
+                "name":"HL_N1_03",
+                "coordinates" : "13.4896262, -88.1930545"
+            },
+            {
+                "name":"HL_N1_04",
+                "coordinates" : "13.4896262, -88.1930545"
+            },
+        ]
     },
     {
         name: "Edificio Gerardo Barrios",
-        coordinates: "13.4900917, -88.1935302"
+        "rooms":[
+            {
+                "name":"Decanaturas | Instituo del graduado",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N2_01",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N2_02",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N2_03",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N2_S1",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N2_S2",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N3_01",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N3_02",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N3_03",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+            {
+                "name":"GB_N3_04",
+                "coordinates" : "13.4900917, -88.1935302"
+            },
+        ]
     },
     {
-        name: "Admisiones Universidad Gerardo Barrios",
-        coordinates: "13.4889261, -88.1932690"
-    },
-    {
-        name: "Auditorio Universidad Gerardo Barrios",
-        coordinates: "13.4885195, -88.1928500"
-    },
-    {
-        name: "Bienestar Estudiantil Universidad Gerardo Barrios",
-        coordinates: "13.4886101, -88.1933524"
+        name: "Edificio de enfermeria Licda. Maria Consuelo Olano",
+        "rooms" : [
+            {
+                "name": "MO_N1_01",
+                "coordinates": "13.4886995, -88.1940295"
+
+            },
+            {
+                "name": "MO_N1_02",
+                "coordinates": "13.4886995, -88.1940295"
+
+            },
+            {
+                "name": "MO_N1_03",
+                "coordinates": "13.4886995, -88.1940295"
+
+            },
+            {
+                "name": "MO_N1_LPE",
+                "coordinates": "13.4886995, -88.1940295"
+
+            },
+            {
+                "name": "MO_N1_LA",
+                "coordinates": "13.4886995, -88.1940295"
+
+            },
+            {
+                "name": "MO_N1_LN",
+                "coordinates": "13.4886995, -88.1940295"
+
+            },
+            {
+                "name": "MO_N1_LMI",
+                "coordinates": "13.4886995, -88.1940295"
+
+            },
+            {
+                "name": "MO_N1_LCQ",
+                "coordinates": "13.4886995, -88.1940295"
+
+            }
+        ]
     }
+    
 ];
 
 // Function to load data from local storage
@@ -45,19 +261,52 @@ async function loadCombinedBuildingData() {
     return [...dataFromLocalStorage, ...buildingsData]; // Combine with the new JavaScript object
 }
 
-// Function to populate the select element from combined data
 async function populateSelectFromCombinedData() {
     const destinationSelect = document.getElementById('destinationSelect');
     const combinedData = await loadCombinedBuildingData();
+
+    // Guarda el valor seleccionado actualmente o la opción por defecto
+    const selectedValue = destinationSelect.value;
+
     destinationSelect.innerHTML = '';
 
+    // Agrega la opción por defecto
+    const defaultOption = document.createElement('option');
+    defaultOption.value = 'default';
+    defaultOption.selected = true;
+    defaultOption.textContent = 'Selecciona una opción:';
+    destinationSelect.appendChild(defaultOption);
+
     combinedData.forEach(location => {
-        const option = document.createElement('option');
-        option.value = location.coordinates;
-        option.textContent = location.name;
-        destinationSelect.appendChild(option);
+        if (location.rooms) {
+            // Es un edificio, agrega el nombre del edificio como un grupo
+            const buildingGroup = document.createElement('optgroup');
+            buildingGroup.label = location.name;
+
+            // Luego, agrega las aulas del edificio al grupo
+            location.rooms.forEach(room => {
+                const roomOption = document.createElement('option');
+                roomOption.value = room.coordinates;
+                roomOption.textContent = room.name;
+                buildingGroup.appendChild(roomOption);
+            });
+
+            destinationSelect.appendChild(buildingGroup);
+        } else {
+            // Es un edificio sin aulas
+            const option = document.createElement('option');
+            option.value = location.coordinates;
+            option.textContent = location.name;
+            destinationSelect.appendChild(option);
+        }
     });
+
+    // Establece el valor seleccionado nuevamente
+    destinationSelect.value = selectedValue;
 }
+
+// Call the function to populate the select element from combined data when the page loads
+populateSelectFromCombinedData();
 
 // Call the function to populate the select element from combined data when the page loads
 populateSelectFromCombinedData();
